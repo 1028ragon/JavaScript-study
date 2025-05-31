3f494ac58ffdf2c3ab45306771dc09a170187282
YouSoYeong <1028ragon@naver.com>
2025-05-31 23:28:34 +0900

    ~EP 19

--- lectures/3_javascript/function.js
+++ lectures/3_javascript/function.js
@@ -12,6 +12,15 @@ let sayHello = function() {
 };
 sayHello();
 
+// 파라미터
+
+function sayHello(name, age) {
+	console.log('이름과 나이를 출력합니.');
+	console.log(`name: ${name}, age: ${age}세`);
+}
+sayHello('짐코딩', 20);
+
+
 // 반환 값
 function sum(number1, number2) {
 	return number1 + number2;
@@ -31,6 +40,42 @@ function aFunc() {
   bFunc();
   console.log('name: ', name);
   console.log('age: ', age);
-  console.log('job: ', job)
+  console.log('job: ', job);
 }
 aFunc();
+
+// 즉시실행함수 
+(function() {
+  console.log('hello')
+})()
+
+(function hello() {
+  console.log('hello')
+})()
+
+// 화살표 표기법
+// 1. function 예약어 생략 가능
+// 2. 함수에 매개변수가 하나라면 괄호도 셍략 가능
+// 3. 함수 바디가 표현식 하나라면 중괄호와 return 문도 생략 가능
+const hello = (name) => {
+	console.log(`Hello ${name}`);
+};
+hello('홍갈동')
+
+// const f3 = function() { return "hello"; }
+// 또는
+const f3 = () => "hello";
+
+// const f4 = function(name) { return `Hello, ${name}`; }
+// 또는
+const f4 = name => `Hello, ${name}`;
+
+// const f5 = function(a, b) { return a + b; }
+// 또는
+const f5 = (a, b) => a + b;
+
+const sum = (number1, number2) => number1 + number2;
+let result1 = sum(5, 3);
+console.log('result: ', result); // result: 8
+
+
---
+++ lectures/3_javascript/object.js
@@ -0,0 +1,6 @@
+const person = {
+	name: '짐코딩',
+	age: 24
+};
+// name이 키(key)이며, 
+// '짐코딩'이 값(value)이다.
